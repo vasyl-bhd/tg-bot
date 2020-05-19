80631cde475d1e9cb6957f515ee53cc063062e95
vasyl.bhd <vasyl.bhd@gmail.com>
2020-05-19 17:10:12 +0300

    Fix timer

--- lvivhotlinebot/src/main/java/com/vasylbhd/lvivhotlinebot/scheduler/LvivHotlineIssuesScheduler.java
+++ lvivhotlinebot/src/main/java/com/vasylbhd/lvivhotlinebot/scheduler/LvivHotlineIssuesScheduler.java
@@ -3,21 +3,25 @@ package com.vasylbhd.lvivhotlinebot.scheduler;
 import com.vasylbhd.lvivhotlinebot.bot.LvivHotlineBot;
 import com.vasylbhd.lvivhotlinebot.dao.InMemoryDao;
 import com.vasylbhd.lvivhotlinebot.model.LvivHotlineResponse;
+import lombok.extern.slf4j.Slf4j;
+import model.Action;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import parser.LvivHotlineIssuesParserImpl;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
-import static java.time.temporal.ChronoUnit.DAYS;
+import java.time.temporal.ChronoUnit;
 
 @Service
+@Slf4j
 public class LvivHotlineIssuesScheduler {
+
+    private static final int CLEAN_UP_DELAY = 3900000;
+    private static final long PARSING_DELAY = 3600000;
 
     private final InMemoryDao inMemoryDao;
     private final LvivHotlineBot lvivHotlineBot;
-
 
     public LvivHotlineIssuesScheduler(
             InMemoryDao inMemoryDao,
@@ -26,20 +30,32 @@ public class LvivHotlineIssuesScheduler {
         this.lvivHotlineBot = lvivHotlineBot;
     }
 
-    @Scheduled(fixedRate = 60 * 60 * 1000)
+    @Scheduled(fixedDelay = PARSING_DELAY)
     void parseAndSend() {
-        lvivHotlineBot.sendMessage("Last id: " + inMemoryDao.getAll());
         new LvivHotlineIssuesParserImpl()
-                .parse(LocalDate.now(), LocalDate.now().plus(1, DAYS))
+                .parse(LocalDate.now(), LocalDate.now().plus(1, ChronoUnit.DAYS))
                 .stream()
+                .filter(this::containAction)
                 .map(LvivHotlineResponse::fromAction)
                 .map(LvivHotlineResponse::toTelegramResponse)
                 .forEach(lvivHotlineBot::sendMessage);
     }
 
-    @Scheduled(fixedRate = 65 * 60 * 1000)
+    private boolean containAction(Action action) {
+        String actionId = action.getId();
+        boolean contains = inMemoryDao.contains(actionId);
+        if (!contains) {
+            inMemoryDao.save(action);
+        }
+        log.info("Saved id {}", actionId);
+        return contains;
+    }
+
+    @Scheduled(fixedDelay = CLEAN_UP_DELAY)
     void cleanUpDb() {
-        inMemoryDao.getAll().entrySet()
-                .removeIf(entry -> entry.getValue().isAfter(LocalDateTime.now()));
+        boolean removed = inMemoryDao.getAll().entrySet()
+                .removeIf(entry -> LocalDateTime.now().isAfter(entry.getValue()));
+        log.info("Is there were removed items: {}", removed);
+
     }
 }
