9efa2876ace928499544a849320de005a7d1eb0a
vasyl.bhd <vasyl.bhd@gmail.com>
2020-05-19 14:31:54 +0300

    Fix scheduling

--- lvivhotlinebot/src/main/java/com/vasylbhd/lvivhotlinebot/scheduler/LvivHotlineIssuesScheduler.java
+++ lvivhotlinebot/src/main/java/com/vasylbhd/lvivhotlinebot/scheduler/LvivHotlineIssuesScheduler.java
@@ -3,16 +3,14 @@ package com.vasylbhd.lvivhotlinebot.scheduler;
 import com.vasylbhd.lvivhotlinebot.bot.LvivHotlineBot;
 import com.vasylbhd.lvivhotlinebot.dao.InMemoryDao;
 import com.vasylbhd.lvivhotlinebot.model.LvivHotlineResponse;
-import model.Action;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import parser.LvivHotlineIssuesParser;
 import parser.LvivHotlineIssuesParserImpl;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.List;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 @Service
 public class LvivHotlineIssuesScheduler {
@@ -28,19 +26,18 @@ public class LvivHotlineIssuesScheduler {
         this.lvivHotlineBot = lvivHotlineBot;
     }
 
-    @Scheduled(fixedDelay = 60 * 60 * 1000)
+    @Scheduled(fixedRate = 60 * 60 * 1000)
     void parseAndSend() {
-        LvivHotlineIssuesParser parser = new LvivHotlineIssuesParserImpl();
-        List<Action> parse = parser.parse(LocalDate.now(), LocalDate.now().plus(1, ChronoUnit.DAYS));
-        parse
+        lvivHotlineBot.sendMessage("Last id: " + inMemoryDao.getAll());
+        new LvivHotlineIssuesParserImpl()
+                .parse(LocalDate.now(), LocalDate.now().plus(1, DAYS))
                 .stream()
-                .filter(i -> !inMemoryDao.contains(i.getId()))
                 .map(LvivHotlineResponse::fromAction)
                 .map(LvivHotlineResponse::toTelegramResponse)
                 .forEach(lvivHotlineBot::sendMessage);
     }
 
-    @Scheduled(fixedDelay = 65 * 60 * 1000)
+    @Scheduled(fixedRate = 65 * 60 * 1000)
     void cleanUpDb() {
         inMemoryDao.getAll().entrySet()
                 .removeIf(entry -> entry.getValue().isAfter(LocalDateTime.now()));
